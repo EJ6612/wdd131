db7fa8c6f7404d87a1c361497ff6ad385c25f92c
ej6612 <ej_6612@outlook.com>
2026-01-15 22:09:33 -0700

    W02 Project

---
+++ mission/dom.js
@@ -0,0 +1,44 @@
+let selectElem = document.getElementById('themetoggle');
+
+selectElem.addEventListener('change', function(){
+
+    let theme = selectElem.value;
+    console.log(theme);
+    if (theme == null)
+    { theme == 'light'}
+    console.log(theme);
+
+    let byui_logo = document.querySelector('img');
+
+    //document.getElementById('css-sheet').href = "mission-css.css";
+
+    if (theme == 'light')
+    {
+        // originally, i would just have two seperate css files for light & dark modes
+        //document.getElementById('css-sheet').href = "mission-css-light.css";
+        // but, this way is better
+
+        byui_logo.setAttribute('src', 'https://wddbyui.github.io/wdd131/images/byui-logo-blue.webp');
+
+        console.log('can u see me?');
+
+        document.querySelector('body').style.backgroundColor = "white";
+        document.querySelector('h1').style.color = "black";
+        document.getElementById('ms-p').style.color = "black";
+        document.querySelector('ul').style.color = "black";
+    }
+
+    else if (theme == 'dark')
+    {
+        byui_logo.setAttribute('src', 'https://wddbyui.github.io/wdd131/images/byui-logo-white.png');
+
+        console.log('but can u see me?');
+
+        //document.getElementById('css-sheet').href = "mission-css-dark.css";
+
+        document.querySelector('body').style.backgroundColor = "black";
+        document.querySelector('h1').style.color = "white";
+        document.getElementById('ms-p').style.color = "white";
+        document.querySelector('ul').style.color = "white";
+    }
+})
