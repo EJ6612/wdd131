b4b18a6d015e6bb8e2ef9c5b25e2f624eabd0748
ej6612 <ej_6612@outlook.com>
2026-01-15 20:06:37 -0700

    W02 DOM and JS activity

---
+++ ponder/dom.js
@@ -0,0 +1,34 @@
+
+let selectElem = document.getElementById('webdevlist');
+selectElem.addEventListener('change', function(){
+    let codeValue = selectElem.value;
+    console.log(codeValue);
+
+    const title = document.querySelector('h1');
+
+    title.textContent = 'Web Page Components';
+
+    // document.querySelector('#topics').style.color = 'red';
+
+    document.getElementById('topics').style.color = 'purple';
+
+    let list = document.querySelector('.list');
+
+    list.style.border = '3px solid black';
+
+    let para = document.querySelector('p');
+
+    //para.style.backgroundColor = 'lightblue';
+
+    para.classList.add('background');
+
+    //document.querySelector('body').classList.add('background');
+
+    const image = document.querySelector('img');
+
+    image.setAttribute('src', 'images/html5_css3_javascript.png');
+
+
+    document.getElementById(codeValue).style.color = 'red';
+})
+                
