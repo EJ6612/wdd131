0e2d83f0205af1cc1c8c6213fb5f4e63f30e20d4
ej6612 <ej_6612@outlook.com>
2026-04-03 20:37:02 -0600

    Alt

--- rentals/catalog/catalog.js
+++ rentals/catalog/catalog.js
@@ -1,6 +1,7 @@
 const items = [
     {
         name: 'Loveseat (Pink)',
+        alt: 'Pink loveseat that fits two people.',
         image: 'images/loveseat_pink.webp',
         description: 'This loveseat is great for weddings and anniversaries.',
         tags: ['seat', 'pink'],
@@ -8,13 +9,15 @@ const items = [
     }, 
     {
         name: 'Table Centerpiece (Bowl)',
+        alt: 'A table with a centerpiece in the center. It is a white bowl with colorful flowers.',
         image: 'images/bowl_centerpiece.webp',
         description: 'These simple centerpieces complement a table very well.',
         tags: ['centerpiece', 'flowers', 'bowl'],
         price: '$10/each'
     },
     {
-        name: 'Ceremony ',
+        name: 'Ceremony Backdrop',
+        alt: 'White backrop made of white drapes (about 10 ft tall). In front are three metal-looking frame arches. Each one has a cluster of flowers attached.',
         image: 'images/ceremony_backdrop.webp',
         description: 'This backdrop includes the white drapes, arch frames, and flowers.',
         tags: ['backdrop', 'frames', 'arches', 'white'],
@@ -22,6 +25,7 @@ const items = [
     },
     {
         name: 'Table Centerpiece (Vases)',
+        alt: 'A table with a centerpiece in the center. It is three vases of varying heights surrounded by flowers.',
         image: 'images/cups_centerpiece.webp',
         description: 'These simple centerpieces complement a table very well.',
         tags: ['centerpiece', 'vase', 'flowers'],
@@ -29,6 +33,7 @@ const items = [
     },
     {
         name: 'Floor Flower Decoration (Small)',
+        alt: 'A metallic cube frame with flowers inside. Said flowers stick out.',
         image: 'images/floor_flowers_small.webp',
         description: '',
         tags: ['floor', 'flowers', 'frame', 'decoration'],
@@ -36,6 +41,7 @@ const items = [
     },
     {
         name: 'Floor Flower Decoration (Large)',
+        alt: 'A cluster of hand arranged flowers on the floor.',
         image: 'images/floor_flowers_large.webp',
         description: '',
         tags: ['floor', 'flowers', 'decoration'],
@@ -43,6 +49,7 @@ const items = [
     },
     {
         name: "Couple's Table (with flowers)",
+        alt: 'A table that fits two people. White tablecloth that drapes down to the floor. A long line of flowers lays across the table.',
         image: 'images/overlaid_flowers_table.webp',
         description: 'Includes flowers and tablecloth.',
         tags: ['table', 'flowers'],
@@ -50,6 +57,7 @@ const items = [
     },
     {
         name: 'Table Centerpiece (Frame)',
+        alt: 'A table with a centerpiece in the center. It is a metallic cube frame with flowers inside. Said flowers stick out.',
         image: 'images/square_centerpiece.webp',
         description: 'These simple centerpieces complement a table very well.',
         tags: ['centerpiece', 'frame', 'flowers'],
@@ -57,6 +65,7 @@ const items = [
     },
     {
         name: 'Wedding Entrance',
+        alt: 'An entrance for people to enter from the sides of a flowerwall to one entrance. Surrounded by white drapes. Programmable lights light up the flowerwall and entrance.',
         image: 'images/wedding_entrance.webp',
         description: 'Includes drapes, programmable lights, flowerwall, flower columns.',
         tags: ['entrance', 'wedding', 'white', 'colors'],
@@ -110,7 +119,7 @@ function tagTemplate(tags) {
 
 function itemTemplate(item) {
 	return `<article class="item-card">
-                <img src="${item.image}">
+                <img src="${item.image}" alt="${item.alt}">
                 <div class="item-info">
                     <div class="item-tags">
                         ${tagTemplate(item.tags)}
